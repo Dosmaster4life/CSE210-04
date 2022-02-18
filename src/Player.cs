7600bb7c9d9830f54908a400f87e17c617621b09
Krisst <krisstopher03@gmail.com>
2022-02-17 21:35:11 -0700

    Player and Score

--- src/Player.cs
+++ src/Player.cs
@@ -1,6 +1,16 @@
 namespace DefaultNamespace;
-
-public class Player
+using raylib;
+public class Player //This class has all the atributes of the player
 {
-    
+ //string color = "red";
+ //int x = 5;
+ //int y = 5;
+ //float radius = 5;
+ //int speed = 10;
+
+ //Below detect if specific keys are being
+ bool IsMouseButtonDown(int W);  //Up
+ bool IsMouseButtonDown(int s); //Down
+ bool IsMouseButtonDown(int a); //left
+ bool IsMouseButtonDown(int D);//Right
 }
