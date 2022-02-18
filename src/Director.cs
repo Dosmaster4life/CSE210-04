4d9ed7fa324a0ce68efeb5fd8f887aef781bee0c
Dosmaster4life <jefferysuperman@gmail.com>
2022-02-17 21:22:09 -0700

    added necessary videoservice to Director.cs

--- src/Director.cs
+++ src/Director.cs
@@ -6,9 +6,15 @@ public class Director
 {
     public void startGame()
     {
-   
-   
-    
-       
+       var vd = new VideoService();
+        Raylib.InitWindow(800, 800, "Greed");
+        while (!Raylib.WindowShouldClose())
+        {
+            vd.draw(null);
+            vd.drawArtifact(50,50,10);
+            
+        }
+
+
     }
 }
--- src/VideoService.cs
+++ src/VideoService.cs
@@ -6,23 +6,20 @@ public class VideoService
 {
     public void startGame()
     {
-        VideoService vd = new VideoService();
-        Raylib.InitWindow(800, 480, "Greed");
-        while (!Raylib.WindowShouldClose())
-        {
-            vd.draw();
-            
-        }
-    }
-    public void draw()
-    {
        
-            Raylib.BeginDrawing();
+    }
+    public void draw(List<Artifact>? objectsToDraw)
+    {
+        Raylib.BeginDrawing();
             Raylib.ClearBackground(Color.WHITE);
 
-            Raylib.DrawText("Hello, world!", 12, 12, 20, Color.BLACK);
 
             Raylib.EndDrawing();
         
+    }
+
+    public void drawArtifact(int x, int y, float radius) // draws an artifact
+    {
+        Raylib.DrawCircle(x,y,radius,Color.BLUE);
     }
 }
