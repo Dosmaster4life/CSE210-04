c47deaf8fbb08716d1734c1a8d0d8db9e0e0775a
HansenDJ <hansendj38@gmail.com>
2022-02-17 20:55:50 -0700

    Added comments of methods to Artifact and Rock classes

--- src/Artifact.cs
+++ src/Artifact.cs
@@ -3,4 +3,7 @@
 public class Artifact
 {
     // Distance formula for circle to detect collision
+    // Compare itself and input of player.
+    // x, y, radius
+    // Player's x, y, and radius are parameters
 }
--- src/Rock.cs
+++ src/Rock.cs
@@ -1,6 +1,6 @@
 namespace DefaultNamespace;
 
-public class Rock
+public class Rock : Artifact
 {
-    
+    // method that decrease score by 100.
 }
