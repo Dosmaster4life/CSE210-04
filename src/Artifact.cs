fdbefe81878a12e128ce94572d0a27f8450eeb42
HansenDJ <hansendj38@gmail.com>
2022-02-17 21:33:07 -0700

    Edited rockImagePath string. Added check Collision method to Artifact.cs

--- src/Artifact.cs
+++ src/Artifact.cs
@@ -2,14 +2,22 @@
 
 public class Artifact
 {
-    // Distance formula for circle to detect collision
-    // Compare itself and input of player.
+    // Compare itself and input of player
     // x, y, radius
     // Player's x, y, and radius are parameters
-    int x1;
-    int y1;
-    int x2;
-    int y2;
+    int x1 = 0;
+    int y1 = -100;
+    float radius1 = 10;
     
-    Math.Sqrt((x2 - x1)^2 + (y2 - y1)^2) - (r2 + r1)
+    // Distance formula for circle to detect collision
+    public bool checkCollision(int x2, int y2, float radius2) {
+        float distance = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2)) - (radius2 + radius1);
+        // Compares if the item is 0 distance away from another object, meaning it collided with an object
+        if (distance <= 0) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
 }
--- src/Rock.cs
+++ src/Rock.cs
@@ -5,7 +5,7 @@ public class Rock : Artifact
     // method that decrease score by 100.
 
     // Variable that contains a file path to an image.
-    private string rockImagePath= "D:\Dylan (User)\Documents\School\CSE210\CSE210-Projects\team\CSE210-04\images\rock_pixel.png";
+    private string rockImagePath= "../CSE210-04/images/rock_pixel.png";
 
     public string getRockImagePath(string rockImage) {
         return rockImagePath;
