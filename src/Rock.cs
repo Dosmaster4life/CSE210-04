bc7dc69e62862e2dbab2ab9ecbfa9e124ae6495e
HansenDJ <hansendj38@gmail.com>
2022-02-17 22:14:00 -0700

    Added decreaseScore method to Rock.cs

--- src/Rock.cs
+++ src/Rock.cs
@@ -2,10 +2,13 @@ namespace DefaultNamespace;
 
 public class Rock : Artifact
 {
-    // method that decrease score by 100.
+    // Decreases score by one
+    public void decreaseScore() {
+        ScoreManager.score -= 100;
+    }
 
-    // Variable that contains a file path to an image.
-    private string rockImagePath= "../CSE210-04/images/rock_pixel.png";
+    // Variable that contains a file path to an image
+    private string rockImagePath = "../CSE210-04/images/rock_pixel.png";
 
     public string getRockImagePath(string rockImage) {
         return rockImagePath;
