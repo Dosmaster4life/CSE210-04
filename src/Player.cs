bd1e6dc06df73d8fcd699844059756871d65cc17
Krisst <krisstopher03@gmail.com>
2022-02-23 12:51:56 -0700

    Added keyboard movement

--- src/Player.cs
+++ src/Player.cs
@@ -9,4 +9,20 @@ public class Player //This class has all the atributes of the player
 
  //Below detect if specific keys are being
 
+        static void Left(string[] args)//this method is to detect if the A key is bineg held down by theh player, if so the palyer's icon should move left. 
+    {
+    if (Raylib.IsKeyPressed(KeyboardKey.KEY_A))
+        {
+
+        }
+    }
+
+    static void Right(string[] args)//this method is to detect if the D key is bineg held down by theh player, if so the palyer's icon should move right. 
+    {
+    if (Raylib.IsKeyPressed(KeyboardKey.KEY_D))
+        {
+
+        }
+    }
+
 }
